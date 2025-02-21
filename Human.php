20a40ac33940a13e8d0639ba1e14a478661b3ef0
Eduardo Batista <carloswe3st@gmail.com>
2025-02-20 23:34:29 -0300

    first commit

---
+++ Human.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "Animal.php";
+
+class Human {
+    public Animal $animal;
+    public string $name;
+    public int $age;
+    public string $address;
+    public int $phone;
+
+    
+
+};
